e0fee39bc9451723c7d8320b4f24d0f3017cc023
NewGr8Player <273221594@qq.com>
2018-06-16 16:28:14 +0800

    Process:基础搭建

---
+++ src/main/java/com/xavier/FastDFSApplication.java
@@ -0,0 +1,15 @@
+package com.xavier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author NewGr8Player
+ */
+@SpringBootApplication
+public class FastDFSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FastDFSApplication.class, args);
+    }
+}
